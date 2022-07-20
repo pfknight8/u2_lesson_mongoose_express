45bcc869a6092f117dad93cf10d30122adf64886
Patrick Knight <pfknight2@gmail.com>
2022-07-20 12:23:54 -0400

    done

---
+++ models/index.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const productSchema = require('./product')
+const brandSchema = require('./brand')
+
+const Product = mongoose.model('Product', productSchema)
+const Brand = mongoose.model('Brand', brandSchema)
+
+module.exports = {
+  Product,
+  Brand
+}
